53461c17d3b8b8bb244ae505d005565b1c2d7549
Andrew Taylor <rolyata47@gmail.com>
2026-01-15 12:59:10 -0600

    Adding exception handling to the dictionary controller

--- src/main/java/homepage/controller/DictionaryController.java
+++ src/main/java/homepage/controller/DictionaryController.java
@@ -2,11 +2,17 @@ package homepage.controller;
 
 import homepage.service.DictionaryService;
 import homepage.model.dictionary.Dictionary;
+import homepage.exception.ErrorResponse;
+import homepage.exception.dictionary.UnableToReadFileException;
+import homepage.exception.dictionary.UnableToParseFileException;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +33,18 @@ public class DictionaryController {
     @PostMapping("/create")
     public Dictionary createDictionaryPost(@RequestParam("subjects") List<String> subjects) {
         return dictionaryService.createDictionary(subjects);
+    }
+
+    @ExceptionHandler(UnableToReadFileException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleUnableToReadFile(UnableToReadFileException ex) {
+        return new ErrorResponse(500, ex.getMessage());
+    }
+
+    @ExceptionHandler(UnableToParseFileException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleUnableToReadFile(UnableToParseFileException ex) {
+        return new ErrorResponse(500, ex.getMessage());
     }
 
 }
